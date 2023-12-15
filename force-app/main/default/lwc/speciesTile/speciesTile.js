46099bb31bb222ec31bd50dd36267f9cf90590db
= <ojeda.alejandro@hotmail.com>
2023-12-15 10:35:16 -0300

    LWC species Tile - basics-

---
+++ force-app/main/default/lwc/speciesTile/speciesTile.js
@@ -0,0 +1,11 @@
+import { LightningElement } from "lwc";
+
+export default class SpeciesTile extends LightningElement {
+  specie = {
+    Name: "Jazmin",
+    Description: "Olorosa y bonita planta trepadora",
+    Image_URL__c:
+      "https://www2.glade.com/~/media/glade/fragrances/fragrance-cues/jasmine-fields.jpg?la=es-ar",
+    Location__c: "Indoors,Outdoors"
+  };
+}
