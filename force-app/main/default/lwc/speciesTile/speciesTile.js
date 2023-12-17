021429bbdfe4c0c57577756ae0b6c047811758d7
= <ojeda.alejandro@hotmail.com>
2023-12-17 10:04:58 -0300

    Icon Outdoors/Indoors

--- force-app/main/default/lwc/speciesTile/speciesTile.js
+++ force-app/main/default/lwc/speciesTile/speciesTile.js
@@ -2,4 +2,16 @@ import { LightningElement, api } from "lwc";
 
 export default class SpeciesTile extends LightningElement {
   @api specie;
+
+  get isOutdoors() {
+    return this.specie.Location__c.includes("Outdoors");
+  }
+
+  get isIndoors() {
+    return this.specie.Location__c.includes("Indoors");
+  }
+
+  navigateToRecordViewPage() {
+    console.log("hola");
+  }
 }
