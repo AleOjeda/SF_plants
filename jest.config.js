ba01d6d7c17c1291e174624adfd6a9c9e476ca64
= <ojeda.alejandro@hotmail.com>
2024-01-15 09:24:03 -0300

    enviorment de test - agregado navigation tests

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,15 @@
-const { jestConfig } = require('@salesforce/sfdx-lwc-jest/config');
-
+const { jestConfig } = require("@salesforce/sfdx-lwc-jest/config");
+const setupFilesAfterEnv = jestConfig.setupFilesAfterEnv || [];
+// setupFilesAfterEnv.push("<rootDir>/jest-sa11y-setup.js");
 module.exports = {
-    ...jestConfig,
-    modulePathIgnorePatterns: ['<rootDir>/.localdevserver']
+  ...jestConfig,
+  moduleNameMapper: {
+    // Jest mocks
+    "^lightning/navigation$":
+      "<rootDir>/force-app/test/jest-mocks/lightning/navigation"
+  },
+  // setupFiles: ["jest-canvas-mock"],
+  setupFilesAfterEnv,
+  testTimeout: 10000,
+  modulePathIgnorePatterns: ["<rootDir>/.localdevserver"]
 };
