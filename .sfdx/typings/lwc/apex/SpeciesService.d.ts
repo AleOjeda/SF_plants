9c61befe2fadd3bb0e2c2e9f834770713fb1359d
= <ojeda.alejandro@hotmail.com>
2024-02-11 18:01:21 -0300

    Species with plants - Deploy Species Services

--- .sfdx/typings/lwc/apex/SpeciesService.d.ts
+++ .sfdx/typings/lwc/apex/SpeciesService.d.ts
@@ -1,3 +1,6 @@
 declare module "@salesforce/apex/SpeciesService.getFilteredSpecies" {
   export default function getFilteredSpecies(param: {searchText: any}): Promise<any>;
 }
+declare module "@salesforce/apex/SpeciesService.getSpeciesWithPlants" {
+  export default function getSpeciesWithPlants(): Promise<any>;
+}
